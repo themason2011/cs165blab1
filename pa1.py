e03507a583d9ff83e31e1b1526f7ada3d6e6f9df
Mason Corey <masoncorey2011@hotmail.com>
2020-04-22 01:19:48 -0700

    Added shit

--- pa1.py
+++ pa1.py
@@ -1,4 +1,14 @@
+import numpy as np
 # Starter code for CS 165B HW2 Spring 2019
+
+def calc_centroid(data):
+
+    cen = np.mean(data, axis = 0)
+    return cen
+
+
+def create_discriminant(centroid_pos, centroid_neg):
+    return
 
 def run_train_test(training_input, testing_input):
     """
@@ -27,6 +37,21 @@ def run_train_test(training_input, testing_input):
             }
     """
 
+    dim = training_input[0][0]
+    num_a = training_input[0][1]
+    num_b = training_input[0][2]
+    num_c = training_input[0][3]
 
-    # TODO: IMPLEMENT
+    classA = training_input[1:1+num_a]
+    classB = training_input[2+num_b:2+num_a+num_b]
+    classC = training_input[2+num_a+num_b:-1]
+
+    centroidA = calc_centroid(classA)
+    centroidB = calc_centroid(classB)
+    centroidB = calc_centroid(classB)
+
+    print(centroidA)
+    print(centroidB)
+    print(centroidC)
+
     pass
